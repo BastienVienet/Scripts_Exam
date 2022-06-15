b1d99a0cac8390bff75f5146c3b195cae7502ff7
Bastien Vienet <bastien.vienet@jobtrek.ch>
2022-06-15 14:52:07 +0200

    Making a .zip file

---
+++ PowerShell/Labo_20220212/Compressed_files_1E7.ps1
@@ -0,0 +1,8 @@
+﻿Import-Module ActiveDirectory
+
+New-Item -Path "C:\" -Name "Workspace" -ItemType "directory"
+New-Item -Path "C:\Workspace" -Name "Compressed_Files" -ItemType "directory"
+
+Get-ADUser -Filter 'Company -like "SoftDev"' -Properties * | Select-Object SamAccountName,UserPrincipalName,Name,GivenName,Surname,Initials,DisplayName,Path,Company,EmailAddress,Title,Department,AccountPassword,ExpirationDate | export-csv -path C:\Workspace\Users_exported.csv
+
+Compress-Archive C:\Workspace\Users_exported.csv -DestinationPath ('C:\Workspace\Compressed_Files\' + (Get-Date -Format dd/MM/yyy) + '.zip')
