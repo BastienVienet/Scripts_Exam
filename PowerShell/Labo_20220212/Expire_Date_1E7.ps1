3243954c7673e54027a609677f4fc7e02eecdc25
Bastien Vienet <bastien.vienet@jobtrek.ch>
2022-06-15 14:51:47 +0200

    Adding an expiration date

---
+++ PowerShell/Labo_20220212/Expire_Date_1E7.ps1
@@ -0,0 +1,15 @@
+﻿Import-Module ActiveDirectory
+
+$csvfile = Import-Csv Users_1E7.csv -Delimiter ","
+
+ForEach ($User in $csvfile)
+{
+    $ExpiryDate = $User.ExpirationDate
+    $username = $User.Username
+
+    
+    if (Get-ADUser -F {SamAccountName -eq $Username})
+    {
+        Set-ADAccountExpiration -Identity $username -DateTime $ExpiryDate
+    } 
+}
