f44516e4abec90a4e3781fd2402a0db4ea6e4010
Bastien Vienet <bastien.vienet@jobtrek.ch>
2022-06-15 14:50:46 +0200

    Creating the Organisation Units

---
+++ PowerShell/Labo_20220212/Create_OU_1E7.ps1
@@ -0,0 +1,8 @@
+﻿$datafromCSV = Import-Csv "Users_1E7.csv" -Delimiter ","
+
+$department = $datafromCSV | Select-Object Department -Unique
+
+for($i=0; $i -lt $department.Length ; $i++)
+{
+    New-ADOrganizationalUnit `    -Name $department[$i].Department `    -Path "DC=ad,DC=private" `
+}
